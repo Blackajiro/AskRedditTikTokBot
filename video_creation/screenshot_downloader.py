0a57c80ef161a1387ba6d1bcbb5e8ec2763f34ed
Anlaky <Lmao>
2022-06-03 15:26:48 +0200

    transparency, bigger text

--- video_creation/screenshot_downloader.py
+++ video_creation/screenshot_downloader.py
@@ -1,10 +1,14 @@
 import os
-
+import cv2
 from playwright.sync_api import sync_playwright
 from pathlib import Path
 from rich.progress import track
 from utils.console import print_step, print_substep
 
+
+def zoom_in(page, times = 5):
+    for _ in range(times):
+        page.keyboard.press("Control++")
 
 def download_screenshots_of_reddit_posts(reddit_object, screenshot_num):
     print_step("Downloading Screenshots")
@@ -16,6 +20,7 @@ def download_screenshots_of_reddit_posts(reddit_object, screenshot_num):
         print_substep("Launching Headless Browser")
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
+        page.set_viewport_size({"width": 600, "height": 600})
         #login
         page.goto("https://www.reddit.com/login/")
         page.fill('#loginUsername', os.getenv("REDDIT_USERNAME"))
@@ -24,6 +29,7 @@ def download_screenshots_of_reddit_posts(reddit_object, screenshot_num):
         page.locator("text=Accept all").click()
         # Get the thread screenshot
         page.goto(reddit_object["thread_url"], timeout=0)
+        page.reload()
 
         if page.locator('[data-testid="content-gate"]').is_visible():
             # This means the post is NSFW and requires to click the proceed button.
@@ -50,10 +56,18 @@ def download_screenshots_of_reddit_posts(reddit_object, screenshot_num):
                 page.locator('[data-testid="content-gate"] button').click()
 
             page.goto(f'https://reddit.com{comment["comment_url"]}')
+
             page.locator(f"#t1_{comment['comment_id']}").screenshot(
                 path=f"assets/png/comment_{idx}.png"
             )
 
             idx += 1
 
+        for l in os.listdir(f"assets/png"):
+            img = cv2.imread(f"assets/png/{l}")
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
+            img[:, :, 3] = 225
+            cv2.imwrite(f"assets/png/{l}", img)
+
+        #TODO: blur, background music, bigger text
         print_substep("Done!", style="bold green")
